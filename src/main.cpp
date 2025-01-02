724a1e1ac35680410ff4738f2533adb321470946
helene <helene.maja@hotmail.dk>
2025-01-02 20:34:12 +0100

    add photoresistor and hardware_adc lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/pwm.h"
+#include "hardware/adc.h"
 
 #include "servo_utils.h"
 
@@ -13,10 +14,18 @@ int main()
     gpio_init(button_pin);
     gpio_set_dir(button_pin, GPIO_IN);
     gpio_pull_up(button_pin);
+
+    // add photoresistor
+    constexpr uint photoResistor_pin {17};
+    adc_init(); //initiliaze adc module
+    adc_gpio_init(photoResistor_pin);
+    adc_select_input(0); // selects ADC0 as the input
+
     initialize_motor();
     
     while (true) 
     {
+        printf("photoresistor value%u\n", adc_read());
         sleep_ms(2);
         volatile bool button_pressed {gpio_get(button_pin) == 0}; // Active low
         move_servo_duration(1000 + 30, button_pressed);
